73697e604d612c5d0dec15774540aded1bca04e7
Thomas <thomaspegg.linck@gmail.com>
2021-02-26 22:22:58 -0300

    update classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 
 public class Main {
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        ClassResults classResults = new ClassResults();
+
+        Student student = new Student();
+
+        student.setName("Jose");
+        student.setGrade(10);
+        student.setResult(FinalResult.approved);
+        classResults.addStudent(student);
+
+        student.setName("Carlos");
+        student.setGrade(5);
+        student.setResult(FinalResult.notApproved);
+        classResults.addStudent(student);
+
+        student.setName("Pedro");
+        student.setGrade(7);
+        student.setResult( FinalResult.approved);
+        classResults.addStudent(student);
+
+
+        classResults.printList();
     }
 }
